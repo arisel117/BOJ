dea22297691b30808bacc1f705e04682ab3cf766
ChanJae Lee <48813631+arisel117@users.noreply.github.com>
2022-02-13 13:59:07 +0900

    Update BOJ 2644.py
    file updated.

--- code/BOJ 2644.py
+++ code/BOJ 2644.py
@@ -6,18 +6,12 @@ import sys
 
 
 class KinshipDegree(object):
-  def __init__(self, num_family, origin, destination, num_link):
+  def __init__(self, num_family, origin, destination, family_dic):
     self.num_family = num_family
     self.origin = origin
     self.destination = destination
-    self.num_link = num_link
-    self.family_dic = {i:-1 for i in range(1, self.num_family + 1)}
+    self.family_dic = family_dic
     self.res = []
-
-  def _make_family_dic(self):
-    for i in range(self.num_link):
-      self.parent, self.child = map(int, sys.stdin.readline().split())
-      self.family_dic[self.child] = self.parent
 
   def _recursive_func(self, x):
     if x != -1:
@@ -32,7 +26,6 @@ class KinshipDegree(object):
       print(-1)
 
   def solve(self):
-    self._make_family_dic()
     self.top_graph_origin = self._recursive_func(self.origin)
     self.top_graph_destination = self._recursive_func(self.destination)
     for top_graph_origin_index, top_graph_destination_index in enumerate(self.top_graph_origin):
@@ -43,9 +36,11 @@ class KinshipDegree(object):
 
 if __name__ == "__main__":
   num_family = int(sys.stdin.readline())
-  family_dic = {i:-1 for i in range(1, num_family + 1)}
   origin, destination = map(int, sys.stdin.readline().split())
   num_link = int(sys.stdin.readline())
-  
-  KinshipDegree_problem = KinshipDegree(num_family, origin, destination, num_link)
+  family_dic = {i:-1 for i in range(1, num_family + 1)}
+  for i in range(num_link):
+    parent, child = map(int, sys.stdin.readline().split())
+    family_dic[child] = parent
+  KinshipDegree_problem = KinshipDegree(num_family, origin, destination, family_dic)
   KinshipDegree_problem.solve()
